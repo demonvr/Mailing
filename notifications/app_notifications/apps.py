6346eaab0bbf1efd5042b3195c2def2c5b99930b
“d_vorobev” <demonvrr@yandex.ru>
2022-11-22 15:35:11 +0300

    создание проекта

---
+++ notifications/app_notifications/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class AppNotificationsConfig(AppConfig):
+    name = 'app_notifications'
+    verbose_name = 'рассылка'
